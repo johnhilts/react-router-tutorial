bfeabfc12196819ec457d9c6dd39d96985073a92
John Hilts <johnhilts@live.com>
2016-06-04 00:09:02 -0700

    Lesson 9
    Index Links
    active only if route is index attribute

--- modules/App.js
+++ modules/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import NavLink from './NavLink'
+import {IndexLink} from 'react-router'
 
 export default React.createClass({
   render() {
     return (
       <div>
-        <h1><NavLink to="/">React Router Tutorial</NavLink></h1>
+        <h1><IndexLink to="/" activeClassName="active">React Router Tutorial</IndexLink></h1>
         <ul role="nav">
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
 
         {this.props.children}
+
+        <div>
+          <NavLink to="/" onlyActiveOnIndex={true}>Go Back Home</NavLink>
+        </div>
 
       </div>
     )
