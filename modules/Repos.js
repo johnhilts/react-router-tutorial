d98b8266ab8f3f5a731828d40165b79319df1b66
John Hilts <johnhilts@live.com>
2016-06-03 23:43:05 -0700

    Lesson 6
    Routes and Link tags with params

--- modules/Repos.js
+++ modules/Repos.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Repos</div>
+    return (
+      <div>
+        <ul>
+          <li><Link to='/repos/johnhilts/weatherinfo'>Weather Info</Link></li>
+          <li><Link to='/repos/jhilts@lampsplus.com/react-for-beginners'>React for Beginners</Link></li>
+        </ul>
+      </div>
+    )
   }
 })
