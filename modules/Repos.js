9ed4c5384f563c557ef6291a9fbd91860dbf479f
John Hilts <johnhilts@live.com>
2016-06-04 00:42:11 -0700

    Lesson 12
    NOTE: Skipped Lesson 11 on production express server tangent
    Added programmatic navigation router.push(path) style logic.
    Added context for routing.
    Linked state to form input fields
    Event handlers

--- modules/Repos.js
+++ modules/Repos.js
@@ -2,13 +2,52 @@ import React from 'react'
 import NavLink from './NavLink'
 
 export default React.createClass({
+
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+
+  getInitialState() {
+    return {
+      inputUserName: '',
+      inputRepoName: '',
+    }
+  },
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = this.state.inputUserName
+    const repo = this.state.inputRepoName
+    const path = `/repos/${userName}/${repo}` // NOTE: literal syntax using backticks - can this be replaced with a normal string?
+    this.context.router.push(path)
+  },
+
+  handleUserNameUpdate(event) {
+    this.setState({
+      inputUserName: event.target.value,
+    })
+  },
+
+  handleRepoNameUpdate(event) {
+    this.setState({
+      inputRepoName: event.target.value,
+    })
+  },
+
   render() {
     return (
       <div>
         <ul>
           <li><NavLink to='/repos/johnhilts/weatherinfo'>Weather Info</NavLink></li>
           <li><NavLink to='/repos/jhilts@lampsplus.com/react-for-beginners'>React for Beginners</NavLink></li>
-        </ul>
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName" value={this.state.inputUserName} onChange={this.handleUserNameUpdate} /> / {' '}
+                <input type="text" placeholder="repo" value={this.state.inputRepoName} onChange={this.handleRepoNameUpdate} />{' '}
+                  <button type="submit">Go</button>
+                </form>
+              </li>
+            </ul>
 
             <div>
               {this.props.children}
